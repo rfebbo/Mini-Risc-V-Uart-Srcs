1bfd1c4b3a64555d8095f38ca1303831260590d7
Jonathan Ting <jting@vols.utk.edu>
2020-06-07 12:58:52 -0400

    Qsort

---
+++ gcc/dataset1.h
@@ -0,0 +1,6 @@
+#define DATA_SIZE 9 
+
+int input_data[DATA_SIZE] = 
+{
+  1, 3, 6, 7, 8, 9, 2, 4, 5
+};
--- gcc/test.c
+++ gcc/test.c
@@ -1,13 +1,15 @@
-#include"print.h"
+#include "uart.h"
 
-int a = 7;
-
-int main(void) {
-	int b = 3;
-	int c = a + b;
-    print(a); 
-	print(b);	
-   	print(c);
-    while(1); 	
+int main(void)
+{
+	char c;
+	int num = 3;
+	char d = "Hi";
+	while (1)
+	{
+		c = uart_read_blocking();
+		uart_write_blocking(c);
+		uart_write_blocking(d);
+	}
 	return 0;
 }
