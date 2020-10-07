3e3a73934092214e0905c3e95d35a2686d07e5c6
Gray <gbruner@vols.utk.edu>
2020-10-07 13:35:12 -0400

    more uart debugging

--- gcc/uartTest.c
+++ gcc/uartTest.c
@@ -1,32 +1,14 @@
 #include"uart.h" 
-#include"print.h" 
+// #include"print.h" 
+#include"utils.h"
 
 int main(void) {
 	uart_init(); 
-	// int c = 65; 
-	// for (c = 65; c < 70; c++) {
-	// 	print(c);
-	// 	uart_write_blocking(c);
-	// }
-	// 	// uart_put(c);
-	// while(1);
-	// int c;
-	// while(1) {
-	// 	c = uart_read_blocking(); 
-	// 	// uart_put(c);
-	// 	print(c);
-	// 	uart_write_blocking(c);
 
-	// }
-/*
 	char h[] = "Hello, world!\r\n"; 
 	// // int strlen = 13; 
 	uart_print(h);
-	// char in[32];
-	// for (int i = 0; i < 32; i++)
-	// 	in[i] = 0;
-	// readline(in, 32); 
-	// print(in);
+
 	char c; 
 	while(1) {
 		c = uart_read_blocking();
