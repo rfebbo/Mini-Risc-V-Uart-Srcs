2c2beb44248ec05779301e282f577e09af568bb8
Jonathan Ting <jting@vols.utk.edu>
2020-11-17 20:49:37 -0600

    New Test

--- gcc/test.c
+++ gcc/test.c
@@ -1,11 +1,12 @@
-#include "print.h"
-#include "utils.h"
+void print(int a)
+{
+    volatile int * p = (int *)0xaaaaa008;
+    *p = a;
+}
 
 int main(void)
 {
-	int num1 = 20;
-	int num2 = -6;
-	print(num1);
-	print(num2);
-	print(divide(num1, num2));
+	int i = 2 + 3;
+	int j = 1 + i;
+	print(j);
 }
