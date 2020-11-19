60afa03282d49424597984c66acd4815bf87f720
Ben Sergent V <bsergent21897@gmail.com>
2020-11-18 22:17:14 -0500

    Update LAA test COE files
    laa_modified_asm.coe should be good for a full
    LAA test once we have UART ready.
    
    laa.coe is a super simplified version of the LAA
    test that doesn't use any predefined arrays but
    instead just loads the registers directly. It's
    good for testing that the register writing for
    the main core even works.

--- gcc/laa.c
+++ gcc/laa.c
@@ -9,46 +9,40 @@ int main(void) {
 	//uart_print(h1);
 	int test = 0;
 
-	int matrixA[9] = { 3, 12, 4,  5, 6, 8, 1, 0, 2 };
-	int matrixB[9] = { 7,  3, 8, 11, 9, 5, 6, 8, 4 };
+	// int matrixA[9] = { 3, 12, 4,  5, 6, 8, 1, 0, 2 };
+	// int matrixB[9] = { 7,  3, 8, 11, 9, 5, 6, 8, 4 };
 	int matrixC[9] = { 0,  0, 0,  0, 0, 0, 0, 0, 0 };
 	// Result should be { 177, 149, 100, 149, 133, 102, 19, 19, 16 }
 
 	//char h2[] = "Loading matrix A\r\n";
 	//uart_print(h2);
-	test = matrixA[0];
-	test = matrixA[1];
-	test = matrixA[2];
-	test = matrixA[3];
-	test = matrixA[4];
-	test = matrixA[5];
-	test = matrixA[6];
-	test = matrixA[7];
-	test = matrixA[8];
+	test = 2;
+	test = 12;
+	test = 4;
+	test = 5;
+	test = 6;
+	test = 8;
+	test = 1;
+	test = 7;
+	test = 2;
 
 	// char h3[] = "Loading matrix B\r\n";
 	// uart_print(h3);
-	test = matrixB[0];
-	test = matrixB[1];
-	test = matrixB[2];
-	test = matrixB[3];
-	test = matrixB[4];
-	test = matrixB[5];
-	test = matrixB[6];
-	test = matrixB[7];
-	test = matrixB[8];
+	test = 7;
+	test = 3;
+	test = 8;
+	test = 11;
+	test = 9;
+	test = 5;
+	test = 6;
+	test = 8;
+	test = 4;
 
 	//char h4[] = "Multiplying matrices...\r\n";
 	//uart_print(h4);
 	int status = 0;
 	do {
 		// Read LAA control reg. into status register
-		status = 0;
-		status = 0;
-		status = 0;
-		status = 0;
-		status = 0;
-		status = 0;
 		status = 0;
 		status = 0;
 	} while (status == 0);
